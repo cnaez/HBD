4dc469e2d982eb7f1719cc2d5ce336b59af1baeb
cena <cenaesmailzadeh@gmail.com>
2024-07-13 17:36:35 +0200

    Updated index.html and playSong.js files: changed background color, removed 'required on Chrome' from start button, and modified audio playback logic.

--- playSong.js
+++ playSong.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   var audio = document.getElementById("myAudio");
+  var playButton = document.getElementById("start");
+
+  // Check if the audio can play automatically
   audio.play().catch(function (error) {
-    console.log("Failed to play audio:", error);
+    console.log("Autoplay prevented:", error);
+    // Show the play button
+    playButton.style.display = "block";
+  });
+
+  // Play audio on button click
+  playButton.addEventListener("click", function () {
+    audio
+      .play()
+      .then(function () {
+        // Hide the play button if the audio plays successfully
+        playButton.style.display = "none";
+      })
+      .catch(function (error) {
+        console.log("Failed to play audio:", error);
+      });
   });
 });
